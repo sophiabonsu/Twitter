f9a669da03af5c7dc0970450e2d4e9e942eed4cc
sophiabonsu <sophiabonsu@gmail.com>
2021-03-05 20:36:54 -0500

    setFavorited

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -14,8 +14,34 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
     
+    var favorited:Bool = false
+    var retweet:Int = -1
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+    }
     
     
+    
+
+    
+    
+    func setFavorited( isFavorited:Bool) {
+        favorited = isFavorited
+        if (favorited) {
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }else{
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+            }
+        
+    
+    }
     
     
     override func awakeFromNib() {
